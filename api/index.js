453627ee2c882fa8027bc7962f0fd60184d847bc
Pablo <pablobustos609@gmail.com>
2022-08-24 02:25:49 -0300

    feat(POST /tasks): POST route for creating a task added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const test = require("./routes/test");
+const tasks = require("./routes/tasks");
 
 // .env config
 require("dotenv").config({path: `${__dirname}/.env`});
@@ -12,11 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/hello", (req, res) => {
-    res.send("task manager app")
-})
-
 // routes
-app.use("/", test)
+app.use("/", test);
+app.use("/tasks", tasks)
 
 app.listen(SERVER_PORT, () => console.log("server listening on port", SERVER_PORT));
